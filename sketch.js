9b13b24dadcd172cd4faf008d42a382ad0bad2b0
Tejas268 <83995723+Tejas268@users.noreply.github.com>
2021-06-21 12:23:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,32 +4,37 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var village, a, ground, ball, basket1, basket2, basket3, wast, ground, wastImage;
+var village, a, ground, ball, dustbin, dustbinIMG, basket1, basket2, basket3, waste, ground, wasteImage;
 
 function preload() {
 
 	village = loadImage("village.jpg");
-	/*wastImage = loadImage("wast.png")*/
+	dustbinIMG = loadImage("dustbin.png")
+	wasteImage = loadImage("waste.png");
 	
 }
 
 function setup() {
-	createCanvas(800, 800);
+	createCanvas(700, 700);
 
 	engine = Engine.create();
 	world = engine.world;
 
-	ball = new Ball(80,750,20,20);
+	ball = new Ball(80,650,20,20);
 
-	ground = new Line(400,770,800,50);
+	ground = new Line(350,670,700,40);
 
-	basket1 = new Line(595,680,20, 120);
-	basket3 = new Line(705,680,20, 120);
-	basket2 = new Line(650,730,130, 20);
+	basket1 = new Line(495,600,14, 120);
+	basket3 = new Line(605,600,14, 120);
+	basket2 = new Line(550,650,130, 14);
 
-	/*wast = createSprite(80,750,50,50);
-	wast.addImage(wastImage);
-	wast.scale = 0.8;*/
+	dustbin = createSprite(550,590,20,20);
+	dustbin.addImage(dustbinIMG);
+	dustbin.scale = 0.5;
+
+	waste = createSprite(80,750,50,50);
+	waste.addImage(wasteImage);
+	waste.scale = 0.06;
 
 	Engine.run(engine);
   
@@ -42,14 +47,14 @@ function draw() {
   rectMode(CENTER);
   Engine.update(engine);
 
-  /*wast.x = ball.x;
-  wast.y = ball.y;*/
-
   ball.display();
   ground.display();
   basket1.display();
   basket2.display();
   basket3.display();
+
+  waste.x = mouseX;
+  waste.y = mouseY;
   
   drawSprites();
 }
